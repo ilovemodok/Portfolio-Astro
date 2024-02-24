d71a17269d242b5d2082166f147b430daa5f891c
ilovemodok <ilovemodok@hotmail.com>
2024-02-25 08:14:46 +0900

    Converting site to Astro framework.
    Reassigning all the images to work with Astro JS

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,5 @@ import { defineConfig } from "astro/config";
 import lottie from "astro-integration-lottie";
 
 export default defineConfig({
-  integrations: [
-    lottie(), // <-- add integration
-  ],
+  integrations: [lottie()],
 });

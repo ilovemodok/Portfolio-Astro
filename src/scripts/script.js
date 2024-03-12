9a6831a2f887e01b8239913284135049d4db9181
ilovemodok <ilovemodok@hotmail.com>
2024-03-12 16:30:28 +0900

    Fixed home links
    Returned home links to just anchor links. Also adjusted where on the page they'll stop.

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -7,21 +7,12 @@ btnNavEl.addEventListener("click", function () {
 });
 
 const allLinks = document.querySelectorAll("nav a");
-// const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
   link.addEventListener("click", function (e) {
     const href = link.getAttribute("href");
 
     e.preventDefault();
-
-    // Scroll back to top
-    if (href === "#") {
-      window.scrollTo({
-        top: 0,
-        behavior: "smooth",
-      });
-    }
 
     // Scroll to other links
     if (href !== "#" && href.startsWith("#")) {
@@ -35,38 +26,6 @@ allLinks.forEach(function (link) {
     }
   });
 });
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   const allLinks = document.querySelectorAll("nav a");
-
-//   allLinks.forEach(function (link) {
-//     link.addEventListener("click", function (e) {
-//       const href = link.getAttribute("href");
-
-//       // Check if the link is pointing to a section within the same page
-//       const isInternalAnchorLink = href.startsWith("#");
-
-//       // If it's an internal anchor link, prevent default behavior and handle smooth scrolling
-//       if (isInternalAnchorLink) {
-//         e.preventDefault();
-
-//         // Determine the target element based on the anchor link
-//         const targetId = href.substring(1); // Remove the "#" symbol
-//         const targetElement = document.getElementById(targetId);
-
-//         // Scroll to the target element with smooth behavior
-//         if (targetElement) {
-//           targetElement.scrollIntoView({ behavior: "smooth" });
-//         }
-
-//         // Close mobile navigation on link click
-//         if (link.classList.contains("main-nav-link")) {
-//           headerEl.classList.toggle("nav-open");
-//         }
-//       }
-//     });
-//   });
-// });
 
 // Sticky Nav
 
@@ -107,12 +66,8 @@ window.addEventListener("scroll", () => {
   if (window.scrollY > scrollPositionToShowElements) {
     backToTop.style.opacity = 1;
     backToTop.style.visibility = "visible";
-    // navBar.style.opacity = 1;
-    // navBar.style.visibility = "visible";
   } else {
-    // navBar.style.opacity = 0;
     backToTop.style.opacity = 0;
-    // backToTop.style.visibility = "hidden";
   }
 });
 

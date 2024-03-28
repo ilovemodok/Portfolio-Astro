01627fdcc51f5526b86f25766a4eeb3b31ca4efb
ilovemodok <ilovemodok@hotmail.com>
2024-03-28 16:24:16 +0900

    Fix bug

--- public/js/gsap.js
+++ public/js/gsap.js
@@ -10,17 +10,17 @@ gsap.registerPlugin(ScrollTrigger);
 
 gsap.from(".mediamite-img", {
   delay: 0.3,
-  duration: 1,
+  duration: 10,
   ease: "power1.out",
   y: "100vw",
-  stagger: 0.5,
+  stagger: 0.7,
   scale: 0.1,
   scrollTrigger: {
     scrub: 1,
-    trigger: "#link-services",
-    start: "top 80%",
-    end: "top 30%",
-    markers: false,
+    trigger: ".services-media",
+    start: "top 90%",
+    end: "top 50%",
+    // markers: true,
     toggleActions: "restart none none none",
     // onEnter onLeave onEnterBack onLeaveBack
   },
@@ -30,4 +30,38 @@ gsap.from(".hero-image-box", {
   opacity: 0,
   duration: 1,
   ease: "power1.out",
+});
+
+// Portfolio Pages
+
+// gsap.from(".opener-splash", {
+//   opacity: 0,
+//   duration: 1,
+//   ease: "power1.out",
+// });
+
+gsap.from(".opener-splash, .opener-splash-mobile", {
+  duration: 1,
+  x: 30,
+  scale: 1.25,
+  opacity: 0,
+  ease: "power1.out",
+});
+
+gsap.utils.toArray(".fade-in, .bento-item").forEach((element) => {
+  gsap.from(element, {
+    opacity: 0, // start at 0 opacity
+    duration: 0.5,
+    scale: 0.95,
+    stagger: 0.3,
+    ease: "power2.out",
+    scrollTrigger: {
+      trigger: element, // use the element as the trigger
+      start: "top 80%", // when the top of the element hits the 80% height of the viewport
+      end: "bottom 20%", // end the animation when the bottom of the element is 20% from the bottom of the viewport
+      toggleActions: "play none none reverse", // play animation forward as it enters and reverse as it leaves
+      // You can experiment with different values for start and end
+      // to trigger the animation at different scroll positions.
+    },
+  });
 });
